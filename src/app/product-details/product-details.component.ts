eaeb7269f7835347fddd2a38299371db30b9fc8e
Shakeel-O <shakeel-o@live.com>
2023-03-08 20:10:19 +0000

    adds product details component

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { Product } from '../shared/interfaces/product';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
+  selectedSize!: string;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.productService.getProduct(productId).subscribe((product) => {
+        this.product = product;
+        this.selectedSize = this.product.sizes[0]; 
+      });
+    }
+  }
+}

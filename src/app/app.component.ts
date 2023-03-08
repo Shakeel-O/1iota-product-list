6e79dffa6dd9c260bcf580cc8df42c9a10d29fa7
Shakeel-O <shakeel-o@live.com>
2023-03-08 18:06:36 +0000

    adds product card to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './services/product.service';
+import { Product } from './shared/interfaces/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '1iota-product-list';
+  product!: Product;
+
+  constructor(private productService: ProductService) { }
+
+  
+  ngOnInit() {
+    this.getProductList();
+  }
+
+  getProductList() {
+    console.log('heeeeeeyy')
+    this.productService.getProductList().subscribe(products => {
+      console.log(products)
+
+      this.product = products[0];
+    });
+  }
 }

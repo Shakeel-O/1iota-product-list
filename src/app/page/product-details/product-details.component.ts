fb871fd691c6fbcb5f986aa2f934da6ccaf9e330
Shakeel-O <shakeel-o@live.com>
2023-03-09 22:47:05 +0000

    moved product service to folder, added recently viewed to product pager

--- src/app/page/product-details/product-details.component.ts
+++ src/app/page/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../../services/product.service';
+import { LocalStorageService } from 'src/app/services/localStorage/localStorage.service';
+import { ProductService } from '../../services/product/product.service';
 import { Product } from '../../shared/interfaces/product';
 
 @Component({
@@ -10,21 +11,26 @@ import { Product } from '../../shared/interfaces/product';
 })
 export class ProductDetailsComponent implements OnInit {
   product!: Product;
+  recentProducts!: Product[];
   selectedSize!: string;
 
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
-  ) { }
+    private productService: ProductService,
+    private localStorageService: LocalStorageService
+    ) { }
 
   ngOnInit(): void {
     const productId = this.route.snapshot.paramMap.get('id');
     if (productId) {
       this.productService.getProduct(productId).subscribe((product) => {
         this.product = product;
-        this.selectedSize = this.product.sizes[0]; 
+        this.selectedSize = this.product.sizes[0];
+        this.localStorageService.addRecentlyViewed(product);
+        this.recentProducts = this.localStorageService.getRecentlyViewed();
       });
+
     }
   }
 }

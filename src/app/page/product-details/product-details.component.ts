661ff43c6741ed2f948117ab72027b2fe0cb2a7b
Shakeel-O <shakeel-o@live.com>
2023-03-09 23:18:27 +0000

    rerenders product page when new product selected

--- src/app/page/product-details/product-details.component.ts
+++ src/app/page/product-details/product-details.component.ts
@@ -7,30 +7,32 @@ import { Product } from '../../shared/interfaces/product';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
-  product!: Product;
+  product?: Product;
   recentProducts!: Product[];
   selectedSize!: string;
-
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
     private localStorageService: LocalStorageService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    const productId = this.route.snapshot.paramMap.get('id');
-    if (productId) {
-      this.productService.getProduct(productId).subscribe((product) => {
-        this.product = product;
-        this.selectedSize = this.product.sizes[0];
-        this.localStorageService.addRecentlyViewed(product);
-        this.recentProducts = this.localStorageService.getRecentlyViewed();
-      });
+    this.route.params.subscribe((params) => {
+      console.log('params', params)
+      const productId = params['id'];
+      if (productId){
+        this.productService.getProduct(productId).subscribe((product) => {
+          this.product = product;
+          this.selectedSize = this.product.sizes[0];
+          this.localStorageService.addRecentlyViewed(product);
+          this.recentProducts = this.localStorageService.getRecentlyViewed();
+        });
+      }
 
-    }
+    });
   }
 }

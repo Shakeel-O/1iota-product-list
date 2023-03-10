9f08d86c739c6ee828b693912c716ebe3c005fec
Shakeel-O <shakeel-o@live.com>
2023-03-10 00:39:30 +0000

    creates cart service

---
+++ src/app/services/cart/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Product } from 'src/app/shared/interfaces/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+constructor() {}
+  private cartItems: Product[] = JSON.parse(sessionStorage.getItem('cartItems') || '[]');
+  private cartItemsSubject = new BehaviorSubject<Product[]>(this.cartItems);
+  cartItems$ = this.cartItemsSubject.asObservable();
+
+
+  addItemToCart(product: Product): void {
+    this.cartItems.push(product);
+    sessionStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+    this.cartItemsSubject.next(this.cartItems);
+  }
+
+  getCartItems(): Product[] {
+      const cartItems = JSON.parse(sessionStorage.getItem('cartItems')  || '[]');
+      return cartItems;
+    }
+
+  getCartSize(): number {
+    this.cartItems = this.getCartItems();
+    return this.cartItems.length;
+  }
+}
